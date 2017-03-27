0e09696fe854c76c407bbc2af482068eb9fa7437
Nick Ciliak <nickciliak@Nicks-MacBook-Pro.local>
2017-03-27 16:37:28 -0500

    more

--- wp-content/themes/slightly/single.php
+++ wp-content/themes/slightly/single.php
@@ -25,7 +25,7 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_format() );
             
-            echo '<div class="row no-pad"><div class="col-xs-12 col-md-10 col-md-offset-1 no-pad"><div class="read-next"><h3>Read this next</h3><h2>' . get_previous_post_link('%link', '%title', TRUE) . '</h2></div></div></div>';
+            echo '<div class="row no-pad"><div class="col-xs-12 col-md-10 col-md-offset-1 no-pad"><div class="read-next"><h5>Read this next</h5><h2>' . get_previous_post_link('%link', '%title', TRUE) . '</h2></div></div></div>';
         
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
