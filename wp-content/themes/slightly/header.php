a3e4637e6ee1ede2756cc6389d572a52e0683ecd
Nick Ciliak <nickciliak@Nicks-MacBook-Pro.local>
2017-09-22 11:26:48 -0500

    Welp I redid the navigation again

--- wp-content/themes/slightly/header.php
+++ wp-content/themes/slightly/header.php
@@ -49,6 +49,7 @@
 			 ?>
 		</div><!-- .site-branding -->
 		<nav id="site-navigation" class="main-navigation" role="navigation">
+            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', '_s' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
       </div>
