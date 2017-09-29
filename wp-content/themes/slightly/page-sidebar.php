2cf45642b9a0f67c02bd0bc4cce264306d038e0c
Nick Ciliak <nickciliak@Nicks-MacBook-Pro.local>
2017-09-29 16:20:03 -0500

    workin on sidebar page template but who knows

--- wp-content/themes/slightly/page-sidebar.php
+++ wp-content/themes/slightly/page-sidebar.php
@@ -1,11 +1,8 @@
 <?php
-/**
- * Template Name: Homepage
- * Description: The front page
- *
- * @package Slightly
+/*
+ * Template Name: Sidebar Page
+ * Description: Page template with right sidebar
  */
-
 get_header(); ?>
 
 <?php $feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
@@ -13,8 +10,16 @@ get_header(); ?>
     <div class="banner-image" style="background-image: url(<?php echo $feat_image; ?>);"></div>
 <?php endif; ?>
 
-  <div class="row">
+<div class="row row--index">
     <div class="col-xs-12">
+			<header class="page-header">
+				<h2 class="h1 page-title"><?php echo get_the_title(); ?></h2>
+			</header><!-- .page-header -->
+    </div>
+</div>
+
+  <div class="row">
+    <div class="col-xs-12 col-sm-7">
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -22,7 +27,7 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', 'sidebar-page' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
@@ -35,11 +40,11 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
     </div>
-    </div> 
-
-<div class="row">
-    <?php recent_posts(3); ?>
-</div>
+  <div class="col-xs-12 col-sm-4 col-sm-offset-1">
+      <?php
+get_sidebar(); ?>
+      </div>
+  </div>
 
 <?php
 get_footer();
