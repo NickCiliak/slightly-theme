04814a38bbe5af10b61642d710ccebcbd621928c
Nick Ciliak <nickciliak@Nicks-MacBook-Pro.local>
2017-12-14 11:16:05 -0600

    add sanitization for body copy color setting

--- inc/customizer.php
+++ inc/customizer.php
@@ -21,6 +21,7 @@ function slightly_customize_register( $wp_customize ) {
      'default'        => '#404040',
         'type'        => 'theme_mod',
         'transport' => 'postMessage',
+        'sanitize_callback' => 'sanitize_hex_color'
     ) );
     $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'bodytext_color', array(
         'label'   => 'Body Text Color',
